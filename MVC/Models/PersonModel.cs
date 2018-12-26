642526892654ae074108360749e42098191a05ba
StanislavBerezin <stanislav_berezin@hotmail.com>
2018-12-26 16:30:17 +1000

    FIrst commit

---
+++ MVC/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+
+        public bool isAlive { get; set; } = true;
+    }
+}
